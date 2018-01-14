38ad6438bad2d7fa3561d0e7c69f8f0976e3465e
pipusana <>
2018-01-14 20:16:48 +0700

    Add  middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} =require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 console.log(process.env.NODE_ENV);
 const app = express();
@@ -103,6 +104,10 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 
